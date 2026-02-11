db29e6122a8a786e2c14549835f835ba753c45e2
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-02-11 20:55:56 +0900

    Initial Commit

---
+++ GNCookBook/GNCookBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  GNCookBookApp.swift
+//  GNCookBook
+//
+//  Created by joe on 2/11/26.
+//
+
+import SwiftUI
+
+@main
+struct GNCookBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
