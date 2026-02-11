58da75bb7ed5cd12887b83b519025be998c89a8d
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-02-11 21:38:05 +0900

    add recipe model and assets
    - add recipe data model and mock data, multiple recipe image assets, and a simple HomeView

--- GNCookBook/GNCookBookApp.swift
+++ GNCookBook/GNCookBookApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct GNCookBookApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }
---
+++ GNCookBook/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  GNCookBook
+//
+//  Created by joe on 2/11/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        VStack {
+            Text("Cooking Book App!")
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
