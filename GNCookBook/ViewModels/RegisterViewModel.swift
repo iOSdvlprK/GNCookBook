3b62052fdb0a2cfcc91f00e14f43a66933328302
iOSdvlprK <110379993+iOSdvlprK@users.noreply.github.com>
2026-02-19 22:40:15 +0900

    add registration view and view model
    - introduce RegisterViewModel (@Observable)
    - update RegisterView to use a @State viewModel and build the registration form

---
+++ GNCookBook/ViewModels/RegisterViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterViewModel.swift
+//  GNCookBook
+//
+//  Created by joe on 2/19/26.
+//
+
+import Foundation
+
+@Observable
+class RegisterViewModel {
+    var username = ""
+    var email = ""
+    var showPassword = false
+    var password = ""
+}
--- GNCookBook/Views/RegisterView.swift
+++ GNCookBook/Views/RegisterView.swift
@@ -8,10 +8,51 @@
 import SwiftUI
 
 struct RegisterView: View {
+    @State var viewModel = RegisterViewModel()
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        VStack {
+        VStack(alignment: .leading) {
+            Text("Username")
+                .font(.system(size: 15))
+            TextField("Username", text: $viewModel.username)
+                .keyboardType(.emailAddress)
+                .textFieldStyle(AuthTextFieldStyle())
+            Text("Email")
+                .font(.system(size: 15))
+            TextField("Email", text: $viewModel.email)
+                .keyboardType(.emailAddress)
+                .textFieldStyle(AuthTextFieldStyle())
+            Text("Password")
+                .font(.system(size: 15))
+            PasswordComponentView(showPassword: $viewModel.showPassword, password: $viewModel.password)
             
+            Button(action: {
+                
+            }, label: {
+                Text("Sign Up")
+                    .font(.system(size: 15, weight: .semibold))
+                    .padding(12)
+                    .foregroundStyle(.white)
+                    .frame(maxWidth: .infinity)
+                    .background(.green)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+            })
+            HStack {
+                Spacer()
+                Text("Already have an account?")
+                    .font(.system(size: 14))
+                Button(action: {
+                    dismiss()
+                }, label: {
+                    Text("Login now")
+                        .font(.system(size: 14, weight: .semibold))
+                })
+                Spacer()
+            }
+            .padding(.top, 20)
         }
+        .padding(.horizontal, 10)
     }
 }
 
